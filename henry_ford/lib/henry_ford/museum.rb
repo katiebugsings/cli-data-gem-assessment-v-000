c9fd9e3bef5934018636af7a9b80f483c57f05cd
Katie Cassell <kcassell944@gmail.com>
2018-06-26 20:11:43 +0000

    museum class

--- henry_ford/lib/henry_ford/museum.rb
+++ henry_ford/lib/henry_ford/museum.rb
@@ -1,5 +1,17 @@
 class Museum
 attr_accessor :tilte, :description, :image_url
+@@all = []
 
+def initialize(description)
+  @description = description
+  @image_url = []
+end
 
+def self.all
+  @@all
+end
+
+def save
+  @@all << self
+  end
 end
